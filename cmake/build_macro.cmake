b22ce288e7b23156d81cc47e570d4ec5b4557a03
Pierre Kestener <pierre.kestener@cea.fr>
2019-10-24 12:06:28 +0200

    update macro to build executable

--- cmake/build_macro.cmake
+++ cmake/build_macro.cmake
@@ -1,12 +1,31 @@
 #
 # define a macro to build an executable
 #
-macro(build_exec name)
+macro(build_exec)
+
+  # get all arguments
+  set (args ${ARGN})
+  list(LENGTH args num_args)
+
+  if(${num_args} GREATER 0)
+    set(name ${ARGV0})
+    list(GET args 0 name)
+  endif()
 
   add_executable(${name} "")
-  target_sources(${name}
-    PUBLIC
-    ${name}.cu)
+  
+  foreach(other_arg IN LISTS args)
+    if (EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${other_arg}.cu")
+      set(filename ${other_arg}.cu)
+    else()
+      set(filename ${other_arg}.cpp)
+    endif()
+
+    target_sources(${name}
+      PUBLIC
+      ${filename})
+  endforeach()
+
   target_include_directories(${name}
     PUBLIC
     ${CMAKE_SOURCE_DIR}/chLib)
